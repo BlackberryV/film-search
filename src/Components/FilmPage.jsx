a6bd6d9fd5147dd262d88f207428982d44d234d5
BlackberryV <viktoria.yakimenko3232@gmail.com>
2022-03-19 13:47:35 +0200

    fix the problem of data loss on reload FilmPage

--- src/Components/FilmPage.jsx
+++ src/Components/FilmPage.jsx
@@ -1,20 +1,22 @@
 import {useLocation} from "react-router-dom";
+import {useState} from "react";
 
 const FilmPage = () => {
-    const location = useLocation()
-    const {film} = location.state
-    console.log(film)
+    const location = useLocation();
+    const {film} = location.state;
+    const [filmData] = useState(film)
+
     return (
         <div className={"page-container"}>
             <div className={"poster"}>
-                <img src={`https://image.tmdb.org/t/p/original/${film.poster}`} alt={`${film.title}`}/>
+                <img src={`https://image.tmdb.org/t/p/original/${filmData.poster}`} alt={`${filmData.title}`}/>
             </div>
             <div className={"info"}>
-                <div className={"title"}>{film.title}</div>
-                <div><span>Overview</span> : {film.overview}</div>
-                <div><span>Rating</span> : {film.rating}</div>
-                <div><span>Original language</span> : {film.rating}</div>
-                <div><span>Adult</span> : {film.adult ? "Yes" : "No"}</div>
+                <div className={"title"}>{filmData.title}</div>
+                <div><span>Overview</span> : {filmData.overview}</div>
+                <div><span>Rating</span> : {filmData.rating}</div>
+                <div><span>Original language</span> : {filmData.rating}</div>
+                <div><span>Adult</span> : {filmData.adult ? "Yes" : "No"}</div>
             </div>
         </div>
     )
